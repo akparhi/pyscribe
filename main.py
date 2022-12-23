7b9ce541b8fc60fd50b94945543b33c7082f8804
akparhi <akp11@iitbbs.ac.in>
2022-12-23 22:49:46 +0530

    whisperx integration

--- main.py
+++ main.py
@@ -1,44 +1,114 @@
 import os
 import time
 from fastapi import FastAPI
-import pytube
-import whisper
+from pydantic import BaseModel
+from enum import Enum
+import whisperx
+import ffmpeg
+from utils import download_file
 
 if not os.path.isdir("files"):
     os.mkdir("files")
 
 
 app = FastAPI()
+language = "en"
+device = "cpu"  # "cuda" if torch.cuda.is_available() else "cpu"
+# whisper models
 # tiny | base | small | medium | large
-tiny_model = whisper.load_model("tiny.en")
-base_model = whisper.load_model("base.en")
+models = {
+    "tiny": whisperx.load_model("tiny.en"),
+    "base": whisperx.load_model("base.en")
+}
+# aligner models
+model_a, metadata = whisperx.load_align_model(
+    language_code=language, device=device)
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"success": True, "data": "Alive"}
 
 
-@app.get("/text")
-async def root():
+class AccuracyEnum(str, Enum):
+    phrase = "phrase"
+    word = "word"
 
+
+class ModelEnum(str, Enum):
+    tiny = "tiny"
+    base = "base"
+
+
+class TranscribeInput(BaseModel):
+    url: str
+    model: ModelEnum = ModelEnum.tiny
+    accuracy: AccuracyEnum = AccuracyEnum.phrase
+    debug: bool = False
+
+
+@app.post("/speech-to-text")
+async def root(input: TranscribeInput):
     # inputs
-    filename = 'test'
-    video = 'https://www.youtube.com/watch?v=MLU4E62c6Vs'
+    accuracy = input.accuracy
+    url = input.url
+    model = models[input.model]
+    debug = input.debug
+
+    # response data
+    data = {
+        "model": input.model,
+        "accuracy": accuracy,
+        "task_durations": {},
+        "duration": "",
+        "phrases": [],
+        "words": []
+    }
 
     # API
+    # 1.download
     tic_download = time.perf_counter()
-    data = pytube.YouTube(video)
-    audio = data.streams.get_audio_only()
-    audio.download(output_path="files", filename=filename+'.wav')
+    src_filename = download_file(url)
     toc_download = time.perf_counter()
+
+    # 2.metadata
+    tic_metadata = time.perf_counter()
+    data["duration"] = ffmpeg.probe(src_filename)["format"]["duration"]
+    toc_metadata = time.perf_counter()
+
+    # 3.transcription
     tic_transcription = time.perf_counter()
-    text = tiny_model.transcribe('files/'+filename+'.wav')
+    result = model.transcribe(src_filename, fp16=False)
+    if debug:
+        data["text"] = result["text"]
+    phrases = [{"b": round(phrase["start"], 1), "e": round(phrase["end"], 1), "p": phrase["text"]}
+               for i, phrase in enumerate(result["segments"])]
+    data["phrases"] = phrases
     toc_transcription = time.perf_counter()
 
+    # 4.alignment
+    tic_alignment = time.perf_counter()
+    if accuracy == "word":
+        result_aligned = whisperx.align(
+            result["segments"], model_a, metadata, src_filename, device)
+        data["phrases"] = [{"b": round(phrase["start"], 1), "e": round(phrase["end"], 1), "p": phrase["text"]}
+                           for i, phrase in enumerate(result_aligned["segments"])]
+        data["words"] = [{"b": round(phrase["start"], 1), "e": round(phrase["end"], 1), "w": phrase["text"]}
+                         for i, phrase in enumerate(result_aligned["word_segments"])]
+        if debug:
+            data["nonaligned_phrases"] = phrases
+    toc_alignment = time.perf_counter()
+
+    # 5.cleanups
+    os.remove(src_filename)
     task_durations = {
-        "0.download": round(toc_download - tic_download, 1),
-        "1.transcription": round(toc_transcription - tic_transcription, 1),
-        "t.total": round(toc_transcription - tic_download, 1)
+        "1.download": round(toc_download - tic_download, 1),
+        "2.metadata": round(toc_metadata - tic_metadata, 1),
+        "3.transcription": round(toc_transcription - tic_transcription, 1),
+        "4.alignment": round(toc_alignment - tic_alignment, 1),
+        "t.total": round(toc_alignment - tic_download, 1)
     }
-    return {"success": True, "taskDurations": task_durations, "text": text['text']}
+    task_durations["t.total"] = round(toc_alignment - tic_download, 1)
+    data["task_durations"] = task_durations
+
+    return {"success": True, "data": data}
---
+++ utils.py
@@ -0,0 +1,54 @@
+import os
+import time
+import pytube
+import re
+import requests
+from requests.exceptions import RequestException
+
+yt_patterns = [
+
+]
+
+
+def is_yt_url(url):
+    youtube_regex = (
+        r'(https?://)?(www\.)?'
+        '(youtube|youtu|youtube-nocookie)\.(com|be)/'
+        '(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})')
+
+    return re.match(youtube_regex, url)
+
+
+def get_filename(url):
+    fragment_removed = url.split("#")[0]  # keep to left of first #
+    query_string_removed = fragment_removed.split("?")[0]
+    scheme_removed = query_string_removed.split("://")[-1].split(":")[-1]
+    if scheme_removed.find("/") == -1:
+        return ""
+    return os.path.basename(scheme_removed)
+
+
+def download_file(url, folder_name='files'):
+    if (is_yt_url(url)):
+        try:
+            data = pytube.YouTube(url)
+            fname = data.streams.get_audio_only().download(output_path=folder_name)
+            return '/'.join(fname.split('/')[-2:])
+        except Exception as e:
+            print(e)
+    else:
+        try:
+            r = requests.get(url)
+            fname = ''
+            if "Content-Disposition" in r.headers.keys():
+                fname = re.findall(
+                    'filename="(.+)"', r.headers["Content-Disposition"])[0]
+            else:
+                fname = get_filename(url)
+
+            saveto = folder_name + '/' + str(time.perf_counter()) + fname
+            f = open(saveto, 'wb')
+            f.write(r.content)
+            return saveto
+        except RequestException as e:
+            print(e)
